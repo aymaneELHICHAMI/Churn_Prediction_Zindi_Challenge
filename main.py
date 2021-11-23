3dc36f1428bf02e0a733906fc7649e77bdf43fc4
aymaneELHICHAMI <aymanelhichami1@gmail.com>
2021-11-23 11:46:01 +0100

    Python File Modified

--- main.py
+++ main.py
@@ -1,6 +1,78 @@
 ######################
 ## Import Libraries ##
 ######################
+# Standard libraries for data analysis:
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.stats import norm, skew
+from scipy import stats
+import statsmodels.api as sm
+
+# sklearn modules for data preprocessing:
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+# sklearn modules for Model Selection:
+from sklearn import svm, tree, linear_model, neighbors
+from sklearn import naive_bayes, ensemble, discriminant_analysis, gaussian_process
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+# sklearn modules for Model Evaluation & Improvement:
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import f1_score, precision_score, recall_score, fbeta_score
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import KFold
+from sklearn import feature_selection
+from sklearn import model_selection
+from sklearn import metrics
+from sklearn.metrics import classification_report, precision_recall_curve
+from sklearn.metrics import auc, roc_auc_score, roc_curve
+from sklearn.metrics import make_scorer, recall_score, log_loss
+from sklearn.metrics import average_precision_score
+from sklearn.decomposition import PCA
+# Standard libraries for data visualization:
+import seaborn as sns
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+color = sns.color_palette()
+import matplotlib.ticker as mtick
+
+from IPython.display import display
+
+pd.options.display.max_columns = None
+from pandas.plotting import scatter_matrix
+from sklearn.metrics import roc_curve
+
+# Miscellaneous Utility Libraries:
+
+import random
+import os
+import re
+import sys
+import timeit
+import string
+import time
+from datetime import datetime
+from time import time
+from dateutil.parser import parse
+import joblib
 
 import os
 import sys
@@ -15,6 +87,9 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from sklearn.base import TransformerMixin
+from lightgbm import LGBMClassifier
+
 import time
 
 from sklearn.svm import SVC
@@ -27,8 +102,7 @@ start_time = time.time()
 my_parser = argparse.ArgumentParser(description='List of arguments to be changed for this challenge')
 my_parser.add_argument('Impute_type', type=str,
                        help='Type of technique used to impute the missing values : Mean - Median - KNN', default='mean')
-# args = my_parser.parse_args()
-# impute_type = args.Impute_type
+#args = my_parser.parse_args()
 
 #################
 ## Import Data ##
@@ -49,14 +123,28 @@ variables_df = variables_df.drop(labels=[0, 1, 2, 3])  # Deleting some unnecessa
 variables_description = {variables_df[variables_df.columns[0]].iloc[i]: variables_df[variables_df.columns[1]].iloc[i]
                          for i in range(len(variables) - 1)}
 
+#print(train_df.describe())
+
+#print(train_df["REGION"].nunique()) #14
+#print(train_df["REGION"].unique())
+#print(train_df["TENURE"].nunique()) #8
+#print(train_df["TENURE"].unique())
+#print(train_df["TOP_PACK"].nunique()) #140
+#print(train_df["TOP_PACK"].unique())
+
+#print(train_df["CHURN"].value_counts()) #403986 Churn(23%) / 1750062 No-Churn(77%)
+#print(train_df.columns)
+#print(train_df.info())
+
 ##########################
 ## Features Engineering ##
 ##########################
 
 # Let's determine columns including missing values
 num_cols_missing = ['MONTANT', 'FREQUENCE_RECH', 'REVENUE', 'ARPU_SEGMENT', 'FREQUENCE', 'DATA_VOLUME',
-                    'ON_NET', 'ORANGE', 'TIGO', 'ZONE1', 'ZONE2', 'REGULARITY', 'FREQ_TOP_PACK']
-cat_cols_missing = ['REGION', 'TENURE', 'TOP_PACK']
+                    'ON_NET', 'ORANGE', 'TIGO', 'ZONE1', 'ZONE2', 'MRG', 'REGULARITY', 'FREQ_TOP_PACK']
+
+cat_cols_missing = ['REGION', 'TENURE', 'TOP_PACK'] #MRG was processed
 
 # Concatenating train and test data to impute values over the whole dataset
 df_concat = pd.concat([train_df, test_df])
@@ -67,23 +155,108 @@ df_concat = df_concat.drop(columns=df_concat.columns[0])  # Deleting the user_id
 df_concat['TENURE'] = [elt[0] for elt in list(df_concat['TENURE'].values[:])]
 df_concat['MRG'] = [0 if elt == 'NO' else 1 for elt in list(df_concat['MRG'].values[:])]
 
+#Features Correlation study
+corr = df_concat[num_cols_missing].corr()
+plt.matshow(corr)
+plt.xticks(range(len(corr.columns)), corr.columns, rotation='vertical')
+plt.yticks(range(len(corr.columns)), corr.columns)
+cb = plt.colorbar()
+cb.ax.tick_params(labelsize=14)
+plt.show()
+
+#df_concat = df_concat.drop(columns=['REVENUE', 'ARPU_SEGMENT'])  # Deleting the target column
+
+#"""
 # Handling missing values in numerical features
 for i in num_cols_missing:
+    #if i not in ['REVENUE', 'ARPU_SEGMENT']:
     df_concat.loc[df_concat.loc[:, i].isnull(), i] = df_concat.loc[:, i].mean() # Mean Technique
     # df_concat.loc[df_concat.loc[:, i].isnull(), i] = df_concat.loc[:, i].median() # Median Technique
+#"""
 
-# Handling missing values in categorical features [KNN imputing with encoding]
+# Handling missing values in categorical features
 # 1/ We can simply delete the 2 categorical features
-df_concat = df_concat.drop(columns=cat_cols_missing)  # Deleting the target column
+#"""
+#df_concat = df_concat.drop(columns=cat_cols_missing)  # Deleting the target column
+#"""
 
-# 2/ We can Impute the missing values
-"""
-imputer = KNNImputer(n_neighbors=3)
-df_concat[cat_cols_missing] = imputer.fit_transform(df_concat[cat_cols_missing])
-"""
+
+# 2/ We can Impute the missing values (Most_Frequent for Categorical and Mean for Numerical)
+class DataFrameImputer(TransformerMixin):
+
+    def __init__(self):
+        """ Impute missing values.
+        Columns of dtype object are imputed with the most frequent value
+        in column.
+        Columns of other types are imputed with mean of column.
+        """
+    def fit(self, X, y=None):
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+            index=X.columns)
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
+
+#"""
+df_concat = DataFrameImputer().fit_transform(df_concat)
 
 #We do need One hot encoding for categorical Features
+def encode_and_bind(original_dataframe, feature_to_encode):
+    dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
+    result = pd.concat([original_dataframe, dummies], axis=1)
+    result = result.drop([feature_to_encode], axis=1)
+    return result
 
+for feature in cat_cols_missing:
+    df_concat = encode_and_bind(df_concat, feature)
+#"""
+
+#KNN Imputing
+"""
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import MinMaxScaler
+
+mm = MinMaxScaler()
+
+def find_category_mappings(df, variable):
+    return {k: i for i, k in enumerate(df[variable].dropna().unique(), 0)}
+
+def integer_encode(df, variable, ordinal_mapping):
+    df[variable] = df[variable].map(ordinal_mapping)
+
+mappin = dict()
+def imputation(df1, cols):
+    df = df1.copy()
+    #Encoding dict &amp; Removing nan
+    #mappin = dict()
+    for variable in cols:
+        mappings = find_category_mappings(df, variable)
+        mappin[variable] = mappings
+
+    #Apply mapping
+    for variable in cols:
+        integer_encode(df, variable, mappin[variable])
+
+    #Minmaxscaler and KNN imputation
+    sca = mm.fit_transform(df)
+    knn_imputer = KNNImputer()
+    knn = knn_imputer.fit_transform(sca)
+    df.iloc[:, :] = mm.inverse_transform(knn)
+    for i in df.columns:
+        df[i] = round(df[i]).astype('int')
+
+    #Inverse transform
+    for i in cols:
+        inv_map = {v: k for k, v in mappin[i].items()}
+        df[i] = df[i].map(inv_map)
+    return df
+
+df_concat = imputation(df_concat, cat_cols_missing)
+print(df_concat.head(4))
+print(df_concat.isna().sum())
+"""
 
 ###########################################
 ## Visualizations for understanding sake ##
@@ -91,11 +264,10 @@ df_concat[cat_cols_missing] = imputer.fit_transform(df_concat[cat_cols_missing])
 
 # What are the different regions we have ?
 # What are the top packs we have ?
-#
+
 ##################################
 ## Preparing data for ML models ##
 ##################################
-
 
 
 # Re-Split after Handling missing values
@@ -123,17 +295,24 @@ X_train = scale.fit_transform(X_train)
 X_valid = scale.transform(X_valid)
 X_test = scale.transform(X_test)
 
+
 ##################################
 ## Preparing data for ML models ##
 ##################################
-#"""
+
 #Training
-clf = LogisticRegression(random_state=0)
+#clf = LogisticRegression(random_state=0)
 #clf = MLPClassifier(activation='relu', max_iter=1000)
 #clf = RandomForestClassifier()
 #clf = HistGradientBoostingClassifier(loss='binary_crossentropy', max_iter=1000)
 
+parameters = {'boosting_type': ('gbdt', 'dart', 'goss'), 'learning_rate': [0.01, 0.005, 0.001], 'n_estimators': [100, 200, 300]}
+lgbm = LGBMClassifier()
+clf = GridSearchCV(lgbm, parameters)
+
 clf.fit(X_train, y_train)
+
+print(clf.best_params_)
 
 #Validation
 y_pred_valid = clf.predict(X_valid)
@@ -154,8 +333,8 @@ print(auc_score_valid)
 y_probas_test = clf.predict_proba(X_test)
 y_proba_churn_test = [y_probas_test[i][1] for i in range(len(y_probas_test))]
 submission_df['CHURN'] = y_proba_churn_test
-submission_df.to_csv('Submission_file_Basic_5.csv', index=None, sep=',')
-#"""
+submission_df.to_csv('Submission_file_Advanced_2.csv', index=None, sep=',')
+
 
 ###################
 ## Main Function ##
@@ -166,7 +345,7 @@ def main():
     #print(df_concat.isna().sum())
     #print(df_concat.head(3))
     #print(df_concat['TENURE'])
-    # print(variables_description)
+    #print(variables_description)
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
